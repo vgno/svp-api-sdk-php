1c9afc9a1b694b99de5a62d29f523ea84e08515e
michmato <michal.matoga@schibsted.pl>
2014-04-04 08:42:58 +0200

    Add methods for array representation input/retrieval

--- library/SvpApi/Entity/EntityAbstract.php
+++ library/SvpApi/Entity/EntityAbstract.php
@@ -28,4 +28,39 @@ abstract class EntityAbstract {
             }
         }
     }
+
+    /**
+     * Fill entity properties with data
+     *
+     * @param array $data
+     */
+    public function exchangeArray($data) {
+        foreach ($this->getEntityProperties() as $property) {
+            if (isset($data[$property])) {
+                $this->{'set' . ucfirst($property)}($data[$property]);
+            }
+        }
+    }
+
+    /**
+     * Get array containing entity values
+     *
+     * @return array
+     */
+    public function getArrayCopy() {
+        $output = [];
+        foreach ($this->getEntityProperties() as $property) {
+            $output[$property] = $this->{'get' . ucfirst($property)}();
+        }
+        return $output;
+    }
+
+    /**
+     * Get list of class' properties
+     *
+     * @return array
+     */
+    private function getEntityProperties() {
+        return array_keys(get_object_vars($this));
+    }
 }
